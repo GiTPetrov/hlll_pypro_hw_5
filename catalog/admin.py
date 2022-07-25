e2b81438cdc5aa2a69fef29df949e3d5cf95b6ba
maksym <max.orel@gmail.com>
2022-07-25 10:47:56 +0200

    hw_9 homework created models, added to admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,43 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Inventories, Features, Properties, VendorCodes
+
+
+class VendorCodesInline(admin.TabularInline):
+    model = VendorCodes
+    extra = 2
+
+
+class FeaturesInline(admin.TabularInline):
+    model = Features
+    extra = 3
+
+
+class PropertiesInline(admin.TabularInline):
+    model = Properties
+    extra = 4
+
+
+class InventoriesAdmin(admin.ModelAdmin):
+    list_display = ('exchange_id', 'inventories_name')
+    # fieldsets = [
+    #     ('Request information', {'fields': ['path', 'request_method']}),
+    #     ('Form data information', {'fields': ['form_data']}),
+    # ]
+    # readonly_fields = ('path', 'request_method', 'form_data')
+    # list_filter = ['timestamp']
+    # search_fields = ['path']
+    inlines = [VendorCodesInline, FeaturesInline]
+
+
+class FeaturesAdmin(admin.ModelAdmin):
+    list_display = ('exchange_id', 'feature_name', 'feature_value')
+
+
+class PropertiesAdmin(admin.ModelAdmin):
+    list_display = ('exchange_id', 'inventories', 'features', 'property_name')
+
+
+admin.site.register(Inventories, InventoriesAdmin)
+admin.site.register(Features, FeaturesAdmin)
+admin.site.register(Properties, PropertiesAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Features(models.Model):
+    exchange_id = models.CharField(max_length=200)
+    feature_name = models.CharField(max_length=200)
+    feature_value = models.CharField(max_length=200)
+
+
+class Inventories(models.Model):
+    exchange_id = models.CharField(max_length=200)
+    inventories_name = models.CharField(max_length=200)
+    vendor_code = models.OneToOneField('VendorCodes', on_delete=models.CASCADE)
+    feature = models.ForeignKey('Features', on_delete=models.CASCADE)
+    property = models.ManyToManyField(
+        Features,
+        through='Properties',
+        through_fields=('inventories', 'features')
+    )
+
+
+class VendorCodes(models.Model):
+    vendor_code_company = models.CharField(max_length=20)
+    vendor_code_supplier = models.CharField(max_length=20)
+
+
+class Properties(models.Model):
+    exchange_id = models.CharField(max_length=200)
+    inventories = models.ForeignKey(Inventories, on_delete=models.CASCADE)
+    features = models.ForeignKey(Features, on_delete=models.CASCADE)
+    property_name = models.CharField(max_length=200)
+
