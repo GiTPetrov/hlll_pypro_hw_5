95e5337be21a4bf442a609a984dcd5197b6ee23c
maksym <max.orel@gmail.com>
2022-07-27 22:16:19 +0200

    hw_9 homework OneToOneField, ForeignKey, ManyToManyField

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,25 +1,43 @@
 from django.contrib import admin
 
-from catalog.models import Inventories, Features, Properties, VendorCodes
+from catalog.models import Group, GroupPath, Inventories, FeatureName, Features, Properties, PropertyName, PropertySet, VendorCodes
+
+
+class GroupPathInline(admin.TabularInline):
+    model = GroupPath
+    extra = 1
 
 
 class VendorCodesInline(admin.TabularInline):
     model = VendorCodes
-    extra = 2
+    extra = 1
 
 
 class FeaturesInline(admin.TabularInline):
     model = Features
-    extra = 3
+    extra = 1
 
 
 class PropertiesInline(admin.TabularInline):
     model = Properties
-    extra = 4
+    extra = 1
+
+
+class PropertySetInline(admin.TabularInline):
+    model = PropertySet
+    extra = 1
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('exchange_id', 'group_name', 'parent')
+
+
+class GroupPathAdmin(admin.ModelAdmin):
+    list_display = ('ancestor', 'descendant', 'path_length')
 
 
 class InventoriesAdmin(admin.ModelAdmin):
-    list_display = ('exchange_id', 'inventories_name')
+    list_display = ('exchange_id', 'inventory_name', 'group')
     # fieldsets = [
     #     ('Request information', {'fields': ['path', 'request_method']}),
     #     ('Form data information', {'fields': ['form_data']}),
@@ -27,17 +45,39 @@ class InventoriesAdmin(admin.ModelAdmin):
     # readonly_fields = ('path', 'request_method', 'form_data')
     # list_filter = ['timestamp']
     # search_fields = ['path']
-    inlines = [VendorCodesInline, FeaturesInline]
+    # inlines = [VendorCodesInline, PropertySetInline, FeaturesInline]
+
+
+class FeatureNameAdmin(admin.ModelAdmin):
+    list_display = ('exchange_id', 'feature_name')
 
 
 class FeaturesAdmin(admin.ModelAdmin):
-    list_display = ('exchange_id', 'feature_name', 'feature_value')
+    list_display = ('name', 'feature_value')
 
 
 class PropertiesAdmin(admin.ModelAdmin):
-    list_display = ('exchange_id', 'inventories', 'features', 'property_name')
+    list_display = ('name', 'property_value')
 
 
+class PropertyNameAdmin(admin.ModelAdmin):
+    list_display = ('property_name',)
+
+
+class PropertySetAdmin(admin.ModelAdmin):
+    list_display = ('exchange_id', 'property_set_name')
+
+
+class VendorCodesAdmin(admin.ModelAdmin):
+    list_display = ('vendor_code_company', 'vendor_code_supplier')
+
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(GroupPath, GroupPathAdmin)
 admin.site.register(Inventories, InventoriesAdmin)
+admin.site.register(FeatureName, FeatureNameAdmin)
 admin.site.register(Features, FeaturesAdmin)
 admin.site.register(Properties, PropertiesAdmin)
+admin.site.register(PropertyName, PropertyNameAdmin)
+admin.site.register(PropertySet, PropertySetAdmin)
+admin.site.register(VendorCodes, VendorCodesAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -1,32 +1,89 @@
 from django.db import models
 
 
-class Features(models.Model):
+class PropertyName(models.Model):
+    property_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.property_name
+
+
+class Properties(models.Model):
+    name = models.ForeignKey('PropertyName', on_delete=models.CASCADE)
+    property_value = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}: {self.property_value}'
+
+
+class PropertySet(models.Model):
+    exchange_id = models.CharField(max_length=200)
+    property_set_name = models.CharField(max_length=200)
+    properties = models.ManyToManyField('Properties')
+
+    def __str__(self):
+        j = str()
+        k = self.properties.all()
+        for i, item in enumerate(k):
+            j += str(item)
+            if i != len(k) - 1:
+                j += '; '
+        return f'{j}'
+
+
+class FeatureName(models.Model):
     exchange_id = models.CharField(max_length=200)
     feature_name = models.CharField(max_length=200)
-    feature_value = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.feature_name
+
+
+class Features(models.Model):
+    name = models.ForeignKey('FeatureName', on_delete=models.CASCADE)
+    feature_value = models.CharField(max_length=200, unique=False)
+
+    def __str__(self):
+        return f'{self.name}: {self.feature_value}'
 
 
 class Inventories(models.Model):
     exchange_id = models.CharField(max_length=200)
-    inventories_name = models.CharField(max_length=200)
-    vendor_code = models.OneToOneField('VendorCodes', on_delete=models.CASCADE)
-    feature = models.ForeignKey('Features', on_delete=models.CASCADE)
-    property = models.ManyToManyField(
-        Features,
-        through='Properties',
-        through_fields=('inventories', 'features')
-    )
+    inventory_name = models.CharField(max_length=200)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    features = models.ManyToManyField('Features')
+    property_set = models.ManyToManyField('PropertySet')
+    vendor_codes = models.OneToOneField('VendorCodes', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.inventory_name
 
 
 class VendorCodes(models.Model):
     vendor_code_company = models.CharField(max_length=20)
     vendor_code_supplier = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.vendor_code_company
 
-class Properties(models.Model):
+
+class GroupPath(models.Model):
+    ancestor = models.ForeignKey('Group', null=True, db_column='ancestor', blank=True, related_name="ancestor", on_delete=models.CASCADE)
+    descendant = models.ForeignKey('Group', null=True, db_column='descendant', blank=True, related_name="descendants", on_delete=models.CASCADE)
+    path_length = models.IntegerField(null=True, db_column='PathLength', blank=True)
+
+    class Meta:
+        db_table = u'GroupPath'
+
+
+class Group(models.Model):
     exchange_id = models.CharField(max_length=200)
-    inventories = models.ForeignKey(Inventories, on_delete=models.CASCADE)
-    features = models.ForeignKey(Features, on_delete=models.CASCADE)
-    property_name = models.CharField(max_length=200)
+    group_name = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', null=True, blank=True, default=None, related_name='children', on_delete=models.CASCADE)
+    group_path = models.ManyToManyField('Group', through='GroupPath', through_fields=('ancestor', 'descendant'))
 
+    class Meta:
+        db_table = u'Group'
+
+    def __str__(self):
+        return self.group_name
